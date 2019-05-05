4454f227cdad690e41fe118285ce7b59914f704b
w-j-gannon <wgannon@austin.utexas.edu>
2019-05-04 21:38:57 -0500

    Updates

---
+++ precache-manifest.217926daee3ba32e3d32987f954aa175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1460161bf5c613b5085e7c604349d18b",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "4ba32503371f93eee87c",
+    "url": "/clickyGame/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clickyGame/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4ba32503371f93eee87c",
+    "url": "/clickyGame/static/js/main.31269153.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
